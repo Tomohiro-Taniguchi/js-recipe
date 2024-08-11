af869bdc18c04adc0f5a1ce54ac39cecd1e9e3b0
Tomohiro-Taniguchi <m241731@hiroshima-u.ac.jp>
2024-08-11 17:38:34 +0900

    [発展]カウンター - 便利なボタンを学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,12 +1,30 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const doubleButton = document.getElementById("double-button")
 
 let count = 0
 
-// ボタンをクリックしたときの処理を登録
+// プラスボタンをクリックしたときの処理を登録
 plusButton.onclick = function () {
   // countを更新
   count += 1
+  // countを表示
+  display.textContent = count
+}
+
+// マイナスボタンをクリックしたときの処理を登録
+minusButton.onclick = function () {
+  // countを更新
+  count -= 1
+  // countを表示
+  display.textContent = count
+}
+
+// ダブルボタンをクリックしたときの処理を登録
+doubleButton.onclick = function () {
+  // countを更新
+  count *= 2
   // countを表示
   display.textContent = count
 }
