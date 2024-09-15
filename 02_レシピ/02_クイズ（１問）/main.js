9a175717103ea16868c55aeff802ccf68daa637a
Tomohiro-Taniguchi <m241731@hiroshima-u.ac.jp>
2024-09-15 16:23:59 +0900

    更新し忘れていたファイルをまとめて保存

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -1,14 +1,12 @@
 const quizText = document.getElementById("quiz-text")
 const quizImage = document.getElementById("quiz-image")
-const choice1 = document.getElementById("choice-1")
-const choice2 = document.getElementById("choice-2")
-const choice3 = document.getElementById("choice-3")
+const button = document.getElementById("choices-container")
 const feedback = document.getElementById("feedback")
 
 // クイズの内容
 const quiz = {
   text: "この星の名前は何でしょう？",
-  image: "Ganymede.jpg",
+  image: "https://i.gyazo.com/546ba7abc3b7e11e105b47737527bf39.png",
   choices: [
     {
       text: "ゴリアテ",
@@ -26,37 +24,35 @@ const quiz = {
   ],
 }
 
+// ボタンを追加する関数
+const addButton = function () {
+  for (let i = 0; i < quiz.choices.length; i++) {
+    const choice = document.createElement("button")
+    choice.id = `choice${i + 1}`
+    choice.textContent = quiz.choices[i].text
+    button.append(choice)
+    choice.onclick = function () {
+      choose(i)
+    }
+  }
+}
+
 // quiz を画面に表示する関数
 const reloadQuiz = function () {
   // 問題文を表示
   quizText.textContent = "Q. " + quiz.text
 
   // 画像を表示
-  quizImage.src = "./images/" + quiz.image
+  quizImage.src = quiz.image
 
-  // 選択肢（ボタン）の中身を表示
-  choice1.textContent = quiz.choices[0].text
-  choice2.textContent = quiz.choices[1].text
-  choice3.textContent = quiz.choices[2].text
+  // 選択肢（ボタン）を追加
+  addButton()
 }
 
 // choiceNumber番目の選択肢を選択
 const choose = function (choiceNumber) {
   // フィードバックを表示
   feedback.textContent = quiz.choices[choiceNumber].feedback
-}
-
-choice1.onclick = function () {
-  // 0 番目の選択肢を選択
-  choose(0)
-}
-choice2.onclick = function () {
-  // 1 番目の選択肢を選択
-  choose(1)
-}
-choice3.onclick = function () {
-  // 2 番目の選択肢を選択
-  choose(2)
 }
 
 // reloadQuiz関数 を実行して、クイズを画面に表示する
